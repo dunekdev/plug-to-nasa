adb64a4d6607b6fa92ffa64b0d96ff5bec679111
dunekdev <losingfireflies23@gmail.com>
2021-10-02 18:59:44 +0200

    Connect app to nasa api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <Search />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Search from '@/components/Search.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Search,
   },
 };
 </script>
